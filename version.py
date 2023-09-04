767afd9effbb37f25f06c64c2f4bfbf37a63f451
rohithb <rohith3085@gmail.com>
2023-09-04 10:01:30 +0530

    python version

---
+++ version.py
@@ -0,0 +1,11 @@
+#2. Write a Python program to find out what version of Python you are using.
+
+
+#Answer
+import sys
+
+print("python version")
+print(sys.version)
+print("python version info")
+print(sys.version_info)
+
