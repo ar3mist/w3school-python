e47789483837e3a7d5fbbb43e0f44b13a7226d78
rohithb <rohith3085@gmail.com>
2023-09-10 22:30:35 +0530

    comma seprated

---
+++ list and tuple.py
@@ -0,0 +1,11 @@
+# Write a Python program that accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.
+
+#Sample data : 3, 5, 7, 23
+
+# Answer
+
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")
+tuple = tuple(list)
+print('List : ', list)
+print('Tuple : ', tuple)
