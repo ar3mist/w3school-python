0d543abb708aa01c5ac439cb74b6c47a8cc57db2
rohithb <rohith3085@gmail.com>
2023-09-11 07:23:43 +0530

    list slicing

---
+++ list.py
@@ -0,0 +1,12 @@
+#8. Write a Python program to display the first and last colors from the following list.
+#color_list = ["Red","Green","White" ,"Black"]
+
+# answer
+
+color = input("Enter any ten  colours using space : ")
+list = color.split(" ")
+print(list)
+
+print(list[0])
+print(list[-2])
+
