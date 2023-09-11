08504fcf26d7ab55818e3f6cd181911e3f1149db
rohithb <rohith3085@gmail.com>
2023-09-11 21:54:58 +0530

    volume of the sphere

---
+++ volumeofthesphere.py
@@ -0,0 +1,11 @@
+#Write a Python program to get the volume of a sphere with radius six.
+
+
+# formula V = 4/3 π r³
+
+radius_of_the_sphere = 6
+
+pi = 3.14
+answer = (4.0/3.0) * pi * (radius_of_the_sphere**3)
+
+print(answer)
